7740a332d52926df01122b4d86042ceec87e3c09
GeorgeT9 <Megageorge1@yandex.ru>
2024-01-28 19:28:14 +0300

    fix: убран запрет создания группы без hadlers - допускается как бесполезный handlerGroup

--- src/handlers/handlerGroup.test.ts
+++ src/handlers/handlerGroup.test.ts
@@ -7,6 +7,7 @@ describe('HandlerGroup', () => {
     const text = 'simple text 12345, 123456,\n12345678, 123-123, 1234567, sfg'
     const rgx = /\b\d{5,7}\b/g
     const h1 = new Handler('h1', rgx, (str) => str)    
+    const h11 = new Handler('h1', rgx, (str) => str)    
     const h2 = new Handler('h2', rgx, () => null)    
     const hg = new HandlerGroup(h1, h2)
 
@@ -18,5 +19,9 @@ describe('HandlerGroup', () => {
         const returnedMap = new Map()
         returnedMap.set('h1', h1.process(text))
         expect(hg.process(text)).toEqual(returnedMap)    
+    })
+
+    it('should throw error if handlers names repeated', () => {
+        expect(() => new HandlerGroup(h1, h2, h11)).toThrow(Error)
     })
 })
