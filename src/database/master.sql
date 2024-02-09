9acbcf098ac443f42aa858c6dcb53691f299670a
GeorgeT9 <Megageorge1@yandex.ru>
2024-02-09 15:31:55 +0300

    dev: коннекты к бд

--- src/database/master.sql
+++ src/database/master.sql
@@ -1,10 +1,8 @@
--- таблица с id items_#
 CREATE TABLE IF NOT EXISTS parts (
     id NUMERIC PRIMARY KEY AUTOINCREMENT
              NOT NULL
 );
 
--- таблица с именами файлов
 CREATE TABLE IF NOT EXISTS files (
     id       NUMERIC PRIMARY KEY AUTOINCREMENT,
     fileName TEXT    UNIQUE
@@ -15,7 +13,6 @@ CREATE TABLE IF NOT EXISTS files (
                      REFERENCES parts (id) ON DELETE CASCADE
 );
 
--- таблица с данными для создания обработчиков
 CREATE TABLE IF NOT EXISTS handlers (
     id       INTEGER PRIMARY KEY AUTOINCREMENT,
     name     TEXT    UNIQUE
@@ -23,33 +20,3 @@ CREATE TABLE IF NOT EXISTS handlers (
     rgx      TEXT    NOT NULL,
     postFunc TEXT
 );
-
-
-
-
--- Создание отдельной БД part_#
-CREATE TABLE IF NOT EXISTS text_boxs (
-    id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    numberLine INTEGER NOT NULL,
-    text       TEXT,
-    fileId     INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS [values] (
-    value     TEXT    NOT NULL,
-    textBoxId INTEGER REFERENCES text_boxs (id) ON DELETE CASCADE
-                      NOT NULL,
-    position  TEXT    NOT NULL
-);
-
-CREATE VIRTUAL TABLE fts USING fts5(text, content=text_boxs, content_rowid=id);
-
-CREATE TRIGGER text_boxs_insert AFTER INSERT ON text_boxs 
-BEGIN
-  INSERT INTO fts(rowid, text) VALUES (new.id, new.text);
-END;
-
-CREATE TRIGGER text_boxs_delete AFTER DELETE ON text_boxs 
-BEGIN
-  INSERT INTO fts(fts, rowid, text) VALUES('delete', old.id, old.text);
-END;
---
+++ src/database/part.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS text_boxs (
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    numberLine INTEGER NOT NULL,
+    text       TEXT,
+    fileId     INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS [values] (
+    value     TEXT    NOT NULL,
+    textBoxId INTEGER REFERENCES text_boxs (id) ON DELETE CASCADE
+                      NOT NULL,
+    position  TEXT    NOT NULL
+);
+
+CREATE VIRTUAL TABLE IF NOT EXISTS fts USING fts5(text, content=text_boxs, content_rowid=id);
+
+CREATE TRIGGER IF NOT EXISTS text_boxs_insert AFTER INSERT ON text_boxs 
+BEGIN
+  INSERT INTO fts(rowid, text) VALUES (new.id, new.text);
+END;
+
+CREATE TRIGGER IF NOT EXISTS text_boxs_delete AFTER DELETE ON text_boxs 
+BEGIN
+  INSERT INTO fts(fts, rowid, text) VALUES('delete', old.id, old.text);
+END;
